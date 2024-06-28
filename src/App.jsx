ae6918fc3c512f9d601d5261c3bfdbd944464ffc
AbelDeveloper9 <161674921+AbelDeveloper9@users.noreply.github.com>
2024-06-27 23:44:08 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ const App = () => {
         throw new Error('Network response was not ok');
       }
       const data = await response.json();
-      setForecastData(data.forecast.forecastday.slice(0));
+      setForecastData(data.forecast.forecastday);
     } catch (error) {
       console.error('Error fetching weather forecast:', error);
     }
